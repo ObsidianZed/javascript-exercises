a39bcc38abbfbaae3d036135baae46b4ae09f2a3
ObsidianZed <zechman4@hotmail.com>
2022-04-26 23:11:42 -0500

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let newString = '';
+    
+    for (i=string.length; i>0; i--) {
+        let letter = string.slice(i-1,i);
+        newString += letter;
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, args) {
+    // arr.splice();
 };
 
 // Do not edit below this line
